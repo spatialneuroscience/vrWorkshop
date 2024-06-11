43028112cca32d114a4a5ee90234f3f08142fd1b
tiffany-raber <tiffany@redelement.media>
2024-06-11 06:58:26 -0700

    Initial commit of Unity project for workshop

---
+++ Assets/vrWorkshop/ObstacleCourse/Scripts/ColorMe.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorMe : MonoBehaviour {
+
+    public Material originalColor;
+    public Material newColor;
+    public bool hasBeenHit; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if bool is false and other Game Object's name is "Burt" in hierarchy...
+        if (hasBeenHit == false && other.name == "Burt")
+            {
+                //Show in console that the collision is occuring
+                Debug.Log("COLLIDED");
+                //Grab component on gameObject and change material to newColor
+                GetComponent<Renderer>().material = newColor;
+                hasBeenHit = true;
+            }
+            //but if bool is true...
+             else if (hasBeenHit == true)
+            {
+                //Change back to original material and change bool back to false
+                GetComponent<Renderer>().material = originalColor;
+                hasBeenHit = false;
+            }
+        }
+    }
+
+
